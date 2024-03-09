939a228a77383698c41d65b0dd0088eb4e0bdc37
nandabi <valerie.nandabi@student.moringaschool.com>
2024-03-09 18:23:55 +0300

    commit message

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,30 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name = 'Morana', job= 'Legal'):
+        self.name = name
+        self.job = job
+
+    def getName(self):
+        return self._name
+    
+    def setName(self, name):
+        if isinstance(name, str) and 0 < len(name) < 26:
+            self._name = name.title()
+            # .title() for converting it into a title
+        else:
+            print ('Name must be string between 1 and 25 characters.')
+            
+
+    name = property(getName, setName)
+
+    def getJob(self):
+        return self._job
+    
+    def setJob (self, job):
+        if job in APPROVED_JOBS:
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
+
+    job = property(getJob, setJob)
